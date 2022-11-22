eeae43d0eaaa1b49d7647422e4a06bb1f0795d39
jonathanleal03 <12jonathan456@gmail.com>
2022-11-22 13:23:25 -0600

    queries and results

---
+++ games-data/sandbox-crud-operations-query.sql
@@ -0,0 +1,6 @@
+--INSERT INTO sandbox(int64_value, bool_value, string_value) VALUES (27372459353, 1, 'whatever you want');
+
+--UPDATE sandbox SET double_value =  -2.3842732398 WHERE id = 125;
+
+--DELETE FROM sandbox WHERE id = 111;
+SELECT * FROM sandbox
