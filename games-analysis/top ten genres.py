7a057199cd42ce632638adfe1c54a51af4416a75
jonathanleal03 <12jonathan456@gmail.com>
2022-12-07 20:35:33 -0600

    add game project

---
+++ games-analysis/top ten genres.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("game-ratings-top-10-genres.csv")
+
+# group by metrics
+df_follow = df.groupby(["genre_name"])["follow_count"].sum().reset_index
+
+df_follow = df_follow.rename(colums = {"follows_count"; "total_follows"})
+
+f_hype= df.groupby(["genre_name"])["hype_count"].sum().reset_index
+
+df_hype = df_hype.rename(colums = {"hype_count"; "total_hypes"})
+
+# analyze data through viziluazation 
+BAR_WIDTH = 0.4 
+
+plt.bar(df_follow.index - BAR_WIDTH / 2, df_follow["total_follow"], color = "blue", label = "number of follows", width = BAR_WIDTH)
+
+plt.bar(df_hype + BAR_WIDTH / 2, df hype["total_hypes"]. color  "red", label = "number of hype", width = BAR_WIDTH)
+
+plt.xticks(df_follow.index, df_follow["genre_name"])
+
+plt.legend(loc = "upper left")
+
+plt.show()
